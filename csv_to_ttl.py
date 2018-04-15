31c941936ffcf03de17cdb5f89207218de2cd676
mvr320 <14887731+mvr320@users.noreply.github.com>
2018-04-15 23:46:39 +0200

    New dataset tooth

--- csv_to_ttl.py
+++ csv_to_ttl.py
@@ -4,15 +4,19 @@ from csv import DictReader
 from rdflib import Dataset, URIRef, Literal, Namespace, RDF, RDFS, OWL, XSD
 from iribaker import to_iri
 
-#in the iribaker file:
-#import urllib.parse as urlparse (suddenly there is urlparse)
-#    if not isinstance(iri, str): (unicode error)
-
 dbp = 'http://dbpedia.org/property/'
 DBP = Namespace(dbp)
 
-filenames = ["lhbt-hulpverlening.csv", "dak-en-thuislozenzorg.csv", "verpleeg-en-verzorgingshuizen.csv","zorg-voor-mensen-met-een-beperking.csv"]
-short = ["lhbt", "dakth", "verz","zorbep"]
+# A namespace for our vocabulary items (schema information, RDFS, OWL classes and properties etc.)
+vocab = 'http://few.vu.nl/~mvr320/KRweb/vocab/'
+VOCAB = Namespace(vocab)
+
+# A namespace for our resources
+data = 'http://few.vu.nl/~mvr320/KRweb/resource'
+DATA = Namespace(data)
+
+filenames = ["dak-en-thuislozenzorg.csv", "tandartsen.csv", "verpleeg-en-verzorgingshuizen.csv","zorg-voor-mensen-met-een-beperking.csv"]
+short = ["dakth", "tooth", "lhbt", "verz","zorbep"]
 for i in range(len(filenames)):
     filename = 'data/'+filenames[i];
     with open(filename,'r', encoding="ISO-8859-1") as csvfile:
@@ -20,13 +24,6 @@ for i in range(len(filenames)):
         csv_contents = [{k: v for k, v in row.items()}
             for row in csv.DictReader(csvfile, skipinitialspace=True, quotechar='"', delimiter=';')]
 
-    # A namespace for our resources
-    data = 'http://few.vu.nl/~mvr320/KRweb/resource'
-    DATA = Namespace(data)
-
-    # A namespace for our vocabulary items (schema information, RDFS, OWL classes and properties etc.)
-    vocab = 'http://few.vu.nl/~mvr320/KRweb/vocab/'
-    VOCAB = Namespace(vocab)
 
     # The URI for our dataset
     url = 'http://few.vu.nl/~mvr320/KRweb/resource/'+short[i]
@@ -48,28 +45,82 @@ for i in range(len(filenames)):
     # Let's iterate over the dictionary, and create some triples
     # Let's pretend we know exactly what the 'schema' of our CSV file is
     for row in csv_contents:
-
-        thing = URIRef(to_iri(url+row['titel']))
-
-        points = row['locatie'][5:].split()
-        lat = points[1][:-1]
-        lng = points[0][1:]
-
-        name = Literal(row['titel'], datatype=XSD['string'])
-        lat = Literal(lat, datatype=XSD['double'])
-        lng = Literal(lng, datatype=XSD['double'])
-        website = Literal(row['internet'], datatype=XSD['string'])
-
-        #if "huisarts" in row.title:
-            #add relation to class huisarts
-
-
+        thing = ""
+        
+        points = ""
+        lat = ""
+        lng = ""
+        
+        name = ""
+        lat = ""
+        lng = ""
+        website = ""
+        
         # All set... we are now going to add the triples to our dataset
-        dataset.add((thing, RDFS['label'], name))
-        dataset.add((thing, DBP['latitude'], lat))
-        dataset.add((thing, DBP['longitude'], lng))
-        dataset.add((thing, VOCAB['website'], website))
-        dataset.add((thing, RDF['type'], VOCAB['Instantie']))
+        if short[i] != "prk":
+            thing = URIRef(to_iri(url+row['titel']))
+            points = row['locatie'][5:].split()
+            lat = points[1][:-1]
+            lng = points[0][1:]
+            name = Literal(row['titel'], datatype=XSD['string'])
+            lat = Literal(lat, datatype=XSD['double'])
+            lng = Literal(lng, datatype=XSD['double'])
+            website = Literal(row['internet'], datatype=XSD['string'])
+            dataset.add((thing, RDFS['label'], name))
+            dataset.add((thing, DBP['latitude'], lat))
+            dataset.add((thing, DBP['longitude'], lng))
+            dataset.add((thing, VOCAB['website'], website))
+            dataset.add((thing, RDF['type'], VOCAB['Instantie']))
+        else:
+            thing = URIRef(to_iri(url+row['Naam']))
+            lat = row['LAT']
+            lng = row['LON']
+            name = Literal(row['Naam'], datatype=XSD['string'])
+            lat = Literal(lat, datatype=XSD['double'])
+            lng = Literal(lng, datatype=XSD['double'])
+            dataset.add((thing, RDFS['label'], name))
+            dataset.add((thing, DBP['latitude'], lat))
+            dataset.add((thing, DBP['longitude'], lng))
+            dataset.add((thing, RDF['type'], VOCAB['park']))
+        if short[i] == "tooth":
+            dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['dentalIssues']))
+            if "Kindertandheelkunde" in row['titel']:
+                dataset.add((thing, VOCAB['providesDentalcareTo'], VOCAB['child']))
+            else:
+                dataset.add((thing, VOCAB['providesDentalcareTo'], VOCAB['adult']))                
+        if short[i] == "prk":
+            dataset.add((thing, VOCAB['providesDentalcareTo'], VOCAB['child']))
+        else:
+            dataset.add((thing, VOCAB['providesDentalcareTo'], VOCAB['adult']))                
+        if short[i] == "dakth":
+            substrdb = ["50", "work","Juttersdok", "HVO-Querido"]
+            substrbw = ["huis", "begeleid", "wonen","discus", "Woon", "HVO-Querido"]
+            substrih = ["jurist", "belangen", "Meldpunt", "HVO-Querido", "Stichting"]
+            substrvl = ["ettafel", "Heils"]
+            for esubstrdb in substrdb:
+                if esubstrdb in row['titel']:
+                    dataset.add((thing, VOCAB['providesReintegration'], VOCAB['dayActivity']))
+                    dataset.add((thing, VOCAB['providesReintegration'], VOCAB['work']))
+                    dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['addictsReintegration']))
+                    dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['homelessReintegration']))
+            for esubstrbw in substrbw:
+                if esubstrbw in row['titel']:
+                    dataset.add((thing, VOCAB['providesReintegration'], VOCAB['livingAccomodation']))
+                    dataset.add((thing, VOCAB['providesReintegration'], VOCAB['sleepLocation']))
+                    dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['addictsReintegration']))
+                    dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['homelessReintegration']))
+            for esubstrih in substrih:
+                if esubstrih in row['titel']:
+                    dataset.add((thing, VOCAB['providesReintegration'], VOCAB['information']))
+                    dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['addictsPrevention']))
+                    dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['addictsReintegration']))
+                    dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['homelessReintegration']))
+                    dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['homelessPrevention']))
+            for esubstrvl in substrvl:
+                if esubstrih in row['titel']:
+                    dataset.add((thing, VOCAB['providesReintegration'], VOCAB['food']))
+            dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['addictsReintegration']))
+            dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['homelessReintegration']))
         if short[i] == "zorbep":
             for substr in ["Woonlocatie", "Woonvoorziening", "Kleinschalig wonen"]:
                 if substr in row['titel']:
@@ -79,8 +130,6 @@ for i in range(len(filenames)):
                 if substr in row['titel']:
                     dataset.add((thing, VOCAB['providesSpecialCare'], VOCAB['day']))
             dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['specialNeedCare']))
-        if short[i] == "dakth":
-            dataset.add((thing, VOCAB['providesInformationAbout'], VOCAB['addictsReintegration']))
             
 
     with open('outputTTL/'+short[i]+'-rdf.trig','wb') as f:
