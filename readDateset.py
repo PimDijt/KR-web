6859ce13596e7ea12013bc7f18d68e4fa444071f
mvr320 <14887731+mvr320@users.noreply.github.com>
2018-05-16 22:43:11 +0200

    statebased

--- readDateset.py
+++ readDateset.py
@@ -1,45 +1,64 @@
 import itertools
-from ruler import *
+from rdfs_rules import *
 
 def printr(listr):
+    tmp = []
     for item in listr:
-        print(item.pair)
+        tmp.append(''.join(item.pair))
+    for item in sorted(tmp):
+        print(item)
 
-functions = [a for a in dir(ruler) if  a.startswith('rdfs')]
+functions = [a for a in dir(rdfs_rules) if  a.startswith('rdfs')]
+
+dynamic_store = {}
+
 with open('testFile.csv','r') as f:
-    tripStoreCl = []
+    tripStoreCl = set()
     lines = f.read().splitlines()
     for line in lines:
-        tripStoreCl.append(ruler(line.split(" ")))
-    print("Standard: ", tripStoreCl)
+        tripStoreCl.add(rdfs_rules(line.split(" ")))
+    print("Standard: ")
+    printr(tripStoreCl)
 
     for perm in list(itertools.permutations(functions)):
         newFact = True
-        tripStore = tripStoreCl[:]
+        tripStore = set(list(tripStoreCl)[:])
         rules=0
-        '''while newFact:
+        while newFact:
             newFact = False
             for func in perm:
                 tripStoreNew = []
-                for tripA in tripStore:
-                    go = True                       
-                    method = getattr(tripA, func)
-                    retlist = method(tripStore)
-                    if retlist:
-                        for ret in retlist:
-                            ret = ruler(ret.split(" "))
-                            for tripX in tripStore:
-                                if tripX.equals(ret):
-                                    go = False
-                            for tripX in tripStoreNew:
-                                if tripX.equals(ret):
-                                    go = False
-                            if go:
-                                tripStoreNew.append(ret)
-                                newFact = True
+                tmp = set()
+                for item in tripStore:
+                    tmp.add(''.join(item.pair))
+                srtTripStore = ''.join(sorted(list(tmp)))
+                if srtTripStore in dynamic_store.keys() and func in dynamic_store[srtTripStore].keys():
+                    tripStoreNew = dynamic_store[srtTripStore][func][:]
+                else:
+                    for tripA in tripStore:
+                        go = True
+                        method = getattr(tripA, func)
+                        retlist = method(tripStore)
+                        if retlist:
+                            for ret in retlist:
+                                ret = rdfs_rules(ret.split(" "))
+                                #if ret in list(tripStore)+tripStoreNew:
+                                #    break
+                                for tripX in list(tripStore)+tripStoreNew:
+                                    if tripX.equals(ret):
+                                        go = False
+                                        break
+                                if go:
+                                    tripStoreNew.append(ret)
+                                    newFact = True
+                    if srtTripStore not in dynamic_store.keys():
+                        dynamic_store[srtTripStore] = {}
+                    if func not in dynamic_store[srtTripStore].keys():
+                        dynamic_store[srtTripStore][func] = []                    
+                    dynamic_store[srtTripStore][func] = tripStoreNew
                 if len(tripStoreNew)>0:
                     rules+=1
                     print("Rule: %s worked"%func)
-                    tripStore += tripStoreNew
-        #printr(tripStore)'''
+                    tripStore = set(list(tripStore)+tripStoreNew)
+        #printr(tripStore)
         print(rules, len(tripStore), perm)
