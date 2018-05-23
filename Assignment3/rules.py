fdb1bbeb070f033709aee42db0c318e28d3bfc7d
mvr320 <14887731+mvr320@users.noreply.github.com>
2018-05-23 19:01:14 +0200

    New rule

--- Assignment3/rules.py
+++ Assignment3/rules.py
@@ -16,7 +16,10 @@ class rules(object):
         self.s = ""
     
     def subs_singleRules(self, current):
-        new = self.rdfs1_allIsDataType(current)
+        new = copy.deepcopy(current)
+        #new = self.rdfs1_allIsDataType(current)
+        new = self.rdfs4a_Resource(new)
+        new = self.rdfs4b_Resource(new)
         return new
 
     def rdfs1_allIsDataType(self, current):
@@ -32,7 +35,7 @@ class rules(object):
                     keys.add(o)
 
         for key in keys:
-            if key not in new['s'].keys():
+            '''if key not in new['s'].keys():
                 new['s'][key] = {}
                 new['s'][key]['p'] = {}
                 new['s'][key]['o'] = {}
@@ -41,7 +44,7 @@ class rules(object):
             if 'rdfs:Datatype'  not in new['s'][key]['o'].keys():
                 new['s'][key]['o']['rdfs:Datatype'] = set()
             new['s'][key]['o']['rdfs:Datatype'].add('rdf:type')
-            new['s'][key]['p']['rdf:type'].add('rdfs:Datatype')
+            new['s'][key]['p']['rdf:type'].add('rdfs:Datatype')'''
             
             if 'rdf:type' not in new['p'].keys():
                 new['p']['rdf:type'] = {}
@@ -54,7 +57,7 @@ class rules(object):
             new['p']['rdf:type']['s'][key].add('rdfs:Datatype')
             new['p']['rdf:type']['o']['rdfs:Datatype'].add(key)
 
-            if 'rdfs:Datatype' not in new['o'].keys():
+            '''if 'rdfs:Datatype' not in new['o'].keys():
                 new['o']['rdfs:Datatype'] = {}
                 new['o']['rdfs:Datatype']['s'] = {}
                 new['o']['rdfs:Datatype']['p'] = {}
@@ -63,7 +66,7 @@ class rules(object):
             if 'rdf:type'       not in new['o']['rdfs:Datatype']['p'].keys():
                 new['o']['rdfs:Datatype']['p']['rdf:type'] = set()
             new['o']['rdfs:Datatype']['s'][key].add('rdf:type')
-            new['o']['rdfs:Datatype']['p']['rdf:type'].add(key)
+            new['o']['rdfs:Datatype']['p']['rdf:type'].add(key)'''
         return new
 
     '''def rdfs2_domain(self, store):
@@ -78,19 +81,77 @@ class rules(object):
     #    for other in store:
     #        if self.p=="rdfs:range" and self.s==other.p:
     #            result.add(other.o+" rdf:type "+self.o)
-    #    return list(result)
+    #    return list(result)'''
 
-    #def rdfs4a_Resource(self, store):
-    #    result = set()
-    #    result.add(self.s+" a rdfs:Datatype")
-    #    return list(result)
+    def rdfs4a_Resource(self, current):
+        new = copy.deepcopy(current)
 
-    #def rdfs4b_Resource(self, store):
-    #    result = set()
-    #    result.add(self.o+" a rdfs:Datatype")
-    #    return list(result)
+        keys = set()
 
-    def rdfs5_subProperty(self, store):
+        for p in new['p'].keys():
+            for s in new['p'][p]['s'].keys():
+                keys.add(s)
+
+        for key in keys:
+            '''if key not in new['s'].keys():
+                new['s'][key] = {}
+                new['s'][key]['p'] = {}
+                new['s'][key]['o'] = {}
+            if 'rdf:type'       not in new['s'][key]['p'].keys():
+                new['s'][key]['p']['rdf:type'] = set()
+            if 'rdfs:Datatype'  not in new['s'][key]['o'].keys():
+                new['s'][key]['o']['rdfs:Datatype'] = set()
+            new['s'][key]['o']['rdfs:Datatype'].add('rdf:type')
+            new['s'][key]['p']['rdf:type'].add('rdfs:Datatype')'''
+            
+            if 'rdf:type' not in new['p'].keys():
+                new['p']['rdf:type'] = {}
+                new['p']['rdf:type']['s'] = {}
+                new['p']['rdf:type']['o'] = {}
+            if key              not in new['p']['rdf:type']['s'].keys():
+                new['p']['rdf:type']['s'][key] = set()
+            if 'rdfs:Resource'  not in new['p']['rdf:type']['o'].keys():
+                new['p']['rdf:type']['o']['rdfs:Datatype'] = set()
+            new['p']['rdf:type']['s'][key].add('rdfs:Resource')
+            new['p']['rdf:type']['o']['rdfs:Resource'].add(key)
+
+            '''if 'rdfs:Datatype' not in new['o'].keys():
+                new['o']['rdfs:Datatype'] = {}
+                new['o']['rdfs:Datatype']['s'] = {}
+                new['o']['rdfs:Datatype']['p'] = {}
+            if key              not in new['o']['rdfs:Datatype']['s'].keys():
+                new['o']['rdfs:Datatype']['s'][key] = set()
+            if 'rdf:type'       not in new['o']['rdfs:Datatype']['p'].keys():
+                new['o']['rdfs:Datatype']['p']['rdf:type'] = set()
+            new['o']['rdfs:Datatype']['s'][key].add('rdf:type')
+            new['o']['rdfs:Datatype']['p']['rdf:type'].add(key)'''
+        
+        return new
+
+
+    def rdfs4b_Resource(self, store):
+        new = copy.deepcopy(current)
+
+        keys = set()
+
+        for p in new['p'].keys():
+            for o in new['p'][p]['o'].keys():
+                keys.add(o)
+
+        for key in keys:
+            if 'rdf:type' not in new['p'].keys():
+                new['p']['rdf:type'] = {}
+                new['p']['rdf:type']['s'] = {}
+                new['p']['rdf:type']['o'] = {}
+            if key              not in new['p']['rdf:type']['s'].keys():
+                new['p']['rdf:type']['s'][key] = set()
+            if 'rdfs:Resource'  not in new['p']['rdf:type']['o'].keys():
+                new['p']['rdf:type']['o']['rdfs:Datatype'] = set()
+            new['p']['rdf:type']['s'][key].add('rdfs:Resource')
+            new['p']['rdf:type']['o']['rdfs:Resource'].add(key)
+        return new
+
+    '''def rdfs5_subProperty(self, store):
         result = set()
         for other in store:
             if self.p=="rdfs:subPropertyOf" and other.p=="rdfs:subPropertyOf" and self.s==other.o:
@@ -146,23 +207,11 @@ class rules(object):
         result = set()
         if self.p=="rdf:type" and self.o=="rdfs:Datatype":
             result.add(self.s+" rdfs:subClassOf rdfs:Literal")
-        return list(result)
-    
-    #def rdfs_bonus1(self, store):
-    #    result = set()
-    #    if self.p=="a":
-    #        result.add(self.s+" rdf:type "+self.o)
-    #    return list(result)
-
-    #def rdfs_bonus2(self, store):
-    #    result = set()
-    #    if self.p=="sc":
-    #        result.add(self.s+" rdfs:subClassOf "+self.o)
-    #    return list(result)
+        return list(result)'''
 
     def equals(self, other):
         #print(self.s,other.s)
         #print(self.p,other.p)
         #print(self.o,other.o)
-        return (self.p==other.p and self.o==other.o and self.s==other.s)'''
+        return (self.p==other.p and self.o==other.o and self.s==other.s)
 
