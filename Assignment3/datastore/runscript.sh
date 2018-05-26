7bd6e5cb7a3b9929e17cc1b4c801476c82990d87
Vasseur <michael.vasseur@vu.nl>
2018-05-26 22:55:50 +0200

    New Hashes

--- Assignment3/datastore/runscript.sh
+++ Assignment3/datastore/runscript.sh
@@ -1,11 +1,13 @@
 for line in `cat ./md5hashes.txt`
 do
 echo $line
+if [ ! -f ../data/$line.nt ]; then
 wget http://download.lodlaundromat.org/$line
 mv $line $line.gz
 gunzip $line.gz
 mv $line ../data/$line.nt
-cd ..
-python parse_data.py
-cd datastore
+#cd ..
+#python parse_data.py
+#cd datastore
+fi
 done
