ef46b04d9fdd0663721ffc62ab1ca253c2640a2e
mvr320 <14887731+mvr320@users.noreply.github.com>
2018-05-27 18:46:34 +0200

    new runscript

---
+++ Assignment3/datastore/runscriptSmaller.sh
@@ -0,0 +1,13 @@
+for line in `cat ./$HOSTNAME/md5hashes.txt`
+do
+echo $line
+if [ ! -f ../data2/$line.nt ]; then
+wget http://download.lodlaundromat.org/$line
+mv $line $line.gz
+gunzip $line.gz
+mv $line ../data2/$line.nt
+cd ..
+python parse_data_smaller.py $line 1> slog$line.log 2>serr$line.err
+cd datastore
+fi
+done
