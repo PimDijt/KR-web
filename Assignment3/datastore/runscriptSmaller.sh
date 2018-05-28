97e25d3e89439893ec57b858a5fa3a697630dc0f
Vasseur <michael.vasseur@vu.nl>
2018-05-28 10:44:00 +0200

    Files were reused

--- Assignment3/datastore/runscriptSmaller.sh
+++ Assignment3/datastore/runscriptSmaller.sh
@@ -2,11 +2,12 @@ for line in `cat ./$HOSTNAME/md5hashes.txt`
 do
 echo $line
 if [ ! -f ../data2/$line.nt ]; then
+cd s
 wget http://download.lodlaundromat.org/$line
 mv $line $line.gz
 gunzip $line.gz
-mv $line ../data2/$line.nt
-cd ..
+mv $line ../../data2/$line.nt
+cd ../..
 python parse_data_smaller.py $line 1> slog$line.log 2>serr$line.err
 cd datastore
 fi
