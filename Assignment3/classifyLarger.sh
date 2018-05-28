600eae7d1fa10ea158c9bb0e24105427b485c5e9
mvr320 <14887731+mvr320@users.noreply.github.com>
2018-05-29 00:52:16 +0200

    actually store the output

--- Assignment3/classifyLarger.sh
+++ Assignment3/classifyLarger.sh
@@ -15,7 +15,7 @@ for line in `ls ./ | cut -d'.' -f1`
 do
     cd ../..
     echo $file $line
-    python apply_rule_larger.py $file $line #1> ./logsDT/l/$file/$line.log 2> ./errDT/l/$file/$line.err
+    python apply_rule_larger.py $file $line 1> ./logsDT/l/$file/$line.log 2> ./errDT/l/$file/$line.err
     cd dataL/$file
 done
 cd ../..
